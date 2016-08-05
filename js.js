1a937d234d0535cded08d1b5f79cc5d24c6f5489
Dawa Sherpa <dawa.sherpa@mihsnyc.org>
2016-08-05 11:42:03 -0400

    made a color changer project

---
+++ js.js
@@ -0,0 +1,37 @@
+$( document ).ready(function() {
+    
+  var color = ["maroon", "purple", "orange", "pink", "violet", "gold", "silver"];
+  
+  
+    
+
+
+   $("#ft").click(function(){
+          var randomNumber = Math.floor(Math.random() * color.length);
+  $('body').css({'background-color': color[randomNumber]}); 
+   
+    $("#Scarlett").click(function(){
+  $('body').css({'background-color': 'yellow'}); 
+   $("#op").click(function(){
+  $('body').css({'background-color': 'blue'}); 
+         $("#erza").click(function(){
+  $('body').css({'background-color': 'maroon'}); 
+ 
+ $("#op").click(function(){
+    $("#abc").append("this color is blue ");
+    
+    $("#erza").click(function(){
+    $("#abc").append("this color is red ");
+});
+    
+    
+    
+    
+    
+});
+});
+});
+});
+});
+
+}); 
